5e009b04c38e8e81a45cb53414f2e6640e034aca
yuya-maruta <ymx08110112@gmail.com>
2020-08-08 14:36:53 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,8 +20,10 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
+    require "date"
+    
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+    
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -35,10 +37,9 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month:(@todays_date + x).month, date:(@todays_date+x).day, plans:plans}
+      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :wday => wdays[(@todays_date + x).wday], :plans => plans}
       @week_days.push(days)
-    end
-
   end
+ end
 end
 
